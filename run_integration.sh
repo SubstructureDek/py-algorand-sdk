09178155419b502e70c0018b6a968ae0c06c18b2
Brice Rising <brice@algorand.com>
2022-01-20 13:09:07 -0500

    Use docker network instead of host network

--- run_integration.sh
+++ run_integration.sh
@@ -7,7 +7,7 @@ pushd $rootdir
 
 # Reset test harness
 rm -rf test-harness
-git clone --single-branch --branch master https://github.com/algorand/algorand-sdk-testing.git test-harness
+git clone --single-branch --branch feature/supportRemoteDocker https://github.com/algorand/algorand-sdk-testing.git test-harness
 
 ## Copy feature files into the project resources
 mkdir -p test/features
@@ -19,12 +19,12 @@ docker build -t py-sdk-testing -f Dockerfile "$(pwd)"
 # Start test harness environment
 ./test-harness/scripts/up.sh
 
-while [ $(curl -sL -w "%{http_code}\\n" "http://localhost:59999/v2/accounts" -o /dev/null --connect-timeout 3 --max-time 5) -ne "200" ]
+while [ $(docker run --network sdk-harness --rm curlimages/curl:7.81.0 -sL -w "%{http_code}\\n" "http://indexer-221-1:8980/v2/accounts" -o /dev/null --connect-timeout 3 --max-time 5) -ne "200" ]
 do
   sleep 1
 done
 
 # Launch SDK testing
 docker run -it \
-     --network host \
+     --network sdk-harness \
      py-sdk-testing:latest 
