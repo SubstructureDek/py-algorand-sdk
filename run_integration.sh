91d8cf0ba47c3fc2110f42f87119e4f18bf7ec0b
Brice Rising <brice@algorand.com>
2022-01-20 13:53:47 -0500

    Add debug line

--- run_integration.sh
+++ run_integration.sh
@@ -18,7 +18,7 @@ docker build -t py-sdk-testing -f Dockerfile "$(pwd)"
 
 # Start test harness environment
 ./test-harness/scripts/up.sh
-
+docker network list
 while [ $(docker run --network test-harness_sdk-harness --rm curlimages/curl:7.81.0 -sL -w "%{http_code}\\n" "http://indexer-221-1:8980/v2/accounts" -o /dev/null --connect-timeout 3 --max-time 5) -ne "200" ]
 do
   sleep 1
